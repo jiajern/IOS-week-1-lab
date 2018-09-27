0564ee6c65aef459d48b31e9921dae6d28367d6b
Alan Lau <alanlau97@gmail.com>
2018-09-26 20:39:37 -0400

    initializing the repo

---
+++ IOS-week-1-lab/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOS-week-1-lab
+//
+//  Created by Alan Lau on 9/26/18.
+//  Copyright © 2018 Alan Lau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
